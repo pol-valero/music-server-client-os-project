5f51c9941e9e9a31fa6f3ede04e61e9b4af5e0bb
Pol Valero <pol.valero.arjona@gmail.com>
2023-11-19 16:43:01 +0100

    The two threads use to listen to Poole/Bowman requests, now share the same list of available Poole servers

--- discoveryProgram/discovery.c
+++ discoveryProgram/discovery.c
@@ -1,12 +1,98 @@
 #include "../globals.h"
 #include "discoveryConfig.h"
 
+typedef struct {
+    char* name;
+    char* ip;
+    int port;
+    int n_connections;
+    int available; //If the poole server disconnects, we will set this to 0
+} PooleStats;
+
+typedef struct {
+    PooleStats* poole_stats;
+    int n_poole_stats;
+} PooleStatsList;
+
 int fd_config;
 
 DiscoveryConfig discovery_config; //This variable has to be global in order to be freed if the program is interrupted by a SIGNAL
 
-void* listenForPooleConnections(/*void* arg*/) {
-    //Thread 1
+PooleStats createPooleStats (Frame frame) {
+
+    PooleStats poole_stats;
+
+    //TODO: TODO: Create new readStringUntilChar that returns the endCharPos + 1 (we will interpret as startingPosition)
+    //We will pass the string by reference to be filled. 
+    
+    //Alternative: do tests with the current method
+    int endCharPos;
+    
+    poole_stats.name = readStringUntilChar(0, frame.data, '&', &endCharPos);
+    poole_stats.ip = readStringUntilChar(endCharPos + 1, frame.data, '&', &endCharPos);
+    poole_stats.port = atoi(readStringUntilChar(endCharPos + 1, frame.data, ' ', &endCharPos)); //Does not matter the endChar we put here
+    poole_stats.n_connections = 0;
+    poole_stats.available = 1;
+
+    return poole_stats;
+}
+
+//TODO: Remove?
+void printPooleStats (PooleStats poole_stats) {
+
+    char* buffer;
+    int buffSize;
+
+    buffSize = asprintf(&buffer, "\nPoole server:\n");
+    printDynStr(buffer, buffSize);
+    free(buffer);
+
+    buffSize = asprintf(&buffer, "%s %s %d %d %d\n", poole_stats.name, poole_stats.ip, poole_stats.port, poole_stats.n_connections, poole_stats.available);
+    printDynStr(buffer, buffSize);
+    free(buffer);
+
+}
+
+//TODO: Remove?
+void printPooleStatsList (PooleStatsList list) {
+
+    char* buffer;
+    int buffSize;
+
+    buffSize = asprintf(&buffer, "\nPoole servers in list:\n");
+    printDynStr(buffer, buffSize);
+    free(buffer);
+
+    for (int i = 0; i < list.n_poole_stats; i++) {
+        buffSize = asprintf(&buffer, "\nPoole\n");
+        printDynStr(buffer, buffSize);
+        free(buffer);
+
+        buffSize = asprintf(&buffer, "%s %s %d %d %d\n", list.poole_stats[i].name, list.poole_stats[i].ip, list.poole_stats[i].port, list.poole_stats[i].n_connections, list.poole_stats[i].available);
+        printDynStr(buffer, buffSize);
+        free(buffer);
+    }
+
+}
+
+void addPooleStatsToList (PooleStatsList *list, PooleStats poole_stats) {
+
+    int n_poole_stats = (*list).n_poole_stats;    
+    
+    (*list).poole_stats = realloc((*list).poole_stats, sizeof(PooleStats) * (n_poole_stats + 1));
+
+    (*list).poole_stats[n_poole_stats] = poole_stats;
+
+    (*list).n_poole_stats++;
+
+}
+
+void* listenForPooleConnections(void* arg) {
+    //Thread
+
+    //PooleStatsList list = *((PooleStatsList*) arg);
+    PooleStatsList* list = (PooleStatsList*) arg;
+
     int fd_client;
     int fd_socket_poole;
 
@@ -15,18 +101,21 @@ void* listenForPooleConnections(/*void* arg*/) {
 
     fd_socket_poole = startServer(discovery_config.port_poole, discovery_config.ip_poole);
 
-    fd_client = accept(fd_socket_poole, (void *) &c_addr, &c_len);
 
     while (1) {
 
-        Frame frame = receiveFrame(fd_client);
+        fd_client = accept(fd_socket_poole, (void *) &c_addr, &c_len);
 
-        char buffer2[100];
-        sprintf(buffer2, "%d %d %s %s", frame.type, frame.header_length, frame.header, frame.data);
-        printx(buffer2);
+        Frame frame = receiveFrame(fd_client);
 
         if (frameIsValid(frame)) {
             sendFrame(0x01, "CON_OK", "", fd_client);
+             
+            PooleStats poole_stats = createPooleStats(frame);
+
+            //addPooleStatsToList(&list, poole_stats);
+            addPooleStatsToList(list, poole_stats);
+
         } else {
             sendFrame(0x01, "CON_KO", "", fd_client);
         }
@@ -37,25 +126,29 @@ void* listenForPooleConnections(/*void* arg*/) {
 
 }
 
-void* listenForBowmanConnections(/*void* arg*/) {
-    //Thread2
+void* listenForBowmanConnections(void* arg) {
+    //Thread
+
     int fd_client;
     int fd_socket_bowman;
+
+    //PooleStatsList list = *((PooleStatsList*) arg);
+    PooleStatsList* list = (PooleStatsList*) arg;
 
     struct sockaddr_in c_addr;
     socklen_t c_len = sizeof(c_addr);
 
     fd_socket_bowman = startServer(discovery_config.port_bowman, discovery_config.ip_bowman);
 
-    fd_client = accept(fd_socket_bowman, (void *) &c_addr, &c_len);
 
     while (1) {
 
-        Frame frame = receiveFrame(fd_client);
+        fd_client = accept(fd_socket_bowman, (void *) &c_addr, &c_len);
 
-        char buffer2[100];
-        sprintf(buffer2, "%d %d %s %s", frame.type, frame.header_length, frame.header, frame.data);
-        printx(buffer2);
+        Frame frame = receiveFrame(fd_client);
+        
+        printPooleStatsList(*list); //TODO: Remove
+        //TODO: If the frame is valid, we will send to Bowman the info of the Poole that has the least connections
 
         if (frameIsValid(frame)) {
             sendFrame(0x01, "CON_OK", "", fd_client);
@@ -71,16 +164,19 @@ void* listenForBowmanConnections(/*void* arg*/) {
 
 void listenForConnections() {
 
+    PooleStatsList list = {.n_poole_stats = 0, .poole_stats = NULL};
+
     pthread_t thread_poole;
     pthread_t thread_bowman;
 
-    pthread_create(&thread_poole, NULL, (void *) listenForPooleConnections, NULL);
-    pthread_create(&thread_bowman, NULL, (void *) listenForBowmanConnections, NULL);
+    pthread_create(&thread_poole, NULL, (void *) listenForPooleConnections,(void*) &list);
+    pthread_create(&thread_bowman, NULL, (void *) listenForBowmanConnections, (void*) &list);
 
     pthread_join(thread_poole, NULL);
     pthread_join(thread_bowman, NULL);
 
-    //TODO: Close file descriptors inside threads 
+    //TODO: Close file descriptors inside threads (We will have to make them global?)
+    //TODO: Free memory of list (We will have to make the list global?)
     
 }
 

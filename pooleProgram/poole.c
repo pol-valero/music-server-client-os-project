1b212e9a263715b84e6d750eb9ab2304b02ffc37
Alex Liu <alex.liu@students.salle.url.edu>
2023-11-19 15:02:26 +0100

    break para ir a comer -.-

--- pooleProgram/poole.c
+++ pooleProgram/poole.c
@@ -5,12 +5,22 @@
 
 #define PATH "pooleProgram/data"
 
+//TODO: si hay tiempo crear una estructura para ficheros y directorios, crear variables globales y manejar-los en terminar ejecucion.
+typedef struct {
+    pthread_t* threadClient;
+    int* fd_client = NULL;
+    int numClients = 0;
+} ClientsSockets;
+
+ClientsSockets Clients;
+////////
+
 int fd_config;
-int fd_client;
+int fd_socket;
 
 ServerConfig server_config; //This variable has to be global in order to be freed if the program is interrupted by a SIGNAL
 
-int fd_socket;
+
 
 void doDiscoveryHandshake() {
 
@@ -43,10 +53,54 @@ void terminateExecution () {
     free(server_config.ip_discovery);
     free(server_config.ip_poole);
 
+    for (int i = 0; i < Clients->numClients; i++){
+        close(Clients->fd_clients[i]);
+        free(Clients->fd_clients[i]);
+    }
+
     close (fd_config);
 
     signal(SIGINT, SIG_DFL);
     raise(SIGINT);
+}
+
+void runServer(int fd_client){
+    Frame receive;
+    do{
+        receive = receiveFrame();
+    } while (strcmp(receive->header, "EXIT"));
+    disconect(fd_client);
+}
+
+void addClient(){
+    struct sockaddr_in c_addr;
+    socklen_t c_len = sizeof(c_addr);
+    int index = Clients->numClients;
+    
+    Clients->fd_client = realloc(Clients->fd_client, sizeof(int) * (index + 1));
+    Clients->threadClient = realloc(Clients->threadClient, sizeof(pthread_t) * (index + 1));
+    (Clients->numClients)++;
+
+    Clients->fd_client[index] = accept(fd_socket, (void *) &c_addr, &c_len);
+    pthread_create(&Clients->threadClient[index], NULL, runServer, Clients->fd_client[index]);
+}
+
+void disconect(int fd_client){
+    int index;
+    for (int i = 0; i < Clients->numClients; i++){
+        if (Clients->fd_client[i] == fd_client){
+            for (int j = i; j < clients->numClients ; j++) {
+                clients->fd_client[i] = clients->fd_client[i + 1];
+            }
+            free(clients->fd_client[clients->numClients]);
+            clients->numClients--;
+            break;
+        }else{
+            printEr("Error: En la desconexion");
+        }
+    }
+    sendFrame(0x06, "CON_OK", "", fd_client);
+    close(fd_client);
 }
 
 char** getFilesName(int *numFiles){
@@ -141,7 +195,7 @@ int main (int argc, char** argv) {
 
     //TODO:  REMOVE THESE LINES, THEY ARE JUST FOR TESTING
 
-    int numFiles;
+    /*int numFiles;
     char** files;
 
     files = getFoldersName(&numFiles);
@@ -165,20 +219,19 @@ int main (int argc, char** argv) {
         free(files);
     }else {
         printx("Error en la lectura de archivos o carpeta vacia");
-    }
+    }*/
+
+    runServer();
 
     ///////////////////
 
     doDiscoveryHandshake();
 
-    //TODO: REMOVE THESE LINES, THEY ARE JUST FOR TESTING
-
-    /*struct sockaddr_in c_addr;
-    socklen_t c_len = sizeof(c_addr);
-
     fd_socket = startServer(server_config.port_poole, server_config.ip_poole);
-    fd_client = accept(fd_socket, (void *) &c_addr, &c_len);*/
-    /////////
+
+    while (1){
+        addClient();
+    }
     
     terminateExecution();
 
